0fcc310c893722805598f383a8f3470a51064e28
GabrielChoque <lgabrielalejandro6480@gmail.com>
2025-02-13 18:11:32 -0400

    prueba criba

--- src/main/java/com/example/pruebaclase/PruebaClaseApplication.java
+++ src/main/java/com/example/pruebaclase/PruebaClaseApplication.java
@@ -7,26 +7,25 @@ public class PruebaClaseApplication {
     /*
     Autor : Gabriel Alejandro Choque Callizaya
      */
-
-    public static void criba(int n ){
+    public static void criba(int n){
         boolean[] esPrimo = new boolean[n+1];
         esPrimo[0] = esPrimo[1] = true;
-        for (int i = 2 ; i * i <= n; i++ ){
+        for(int i = 2 ; i * i <= n; i++){
             if (!esPrimo[i]){
                 for (int j = i * i ; j <= n; j+=i){
-                    esPrimo[j]= true;
+                    esPrimo[j] = true;
                 }
             }
         }
-        System.out.println("n: " + n);
-        for (int j = 2 ; j <= n; j++){
-            if (!esPrimo[j]){
-                System.out.println(j + " ");
+
+        for (int i = 2 ; i <= n ; i++ ){
+            if (!esPrimo[i]){
+                System.out.print(" " + i);
             }
         }
     }
     public static void main(String[] args) {
-        int n = 100000000;
-        criba(n);
+      int n = 1000000 ;
+      criba(n);
     }
 }
